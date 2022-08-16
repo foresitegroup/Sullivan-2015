447a8b5f1b8ca2631d6f8dbaa84f29f2064e905a
foresitegroup <mark@foresitegrp.com>
2022-08-16 09:48:45 -0500

    Update gallery

--- gallery.php
+++ gallery.php
@@ -9,6 +9,11 @@ include "header.php";
 
 <h3>GALLERY</h3>
 
+<a class="fancybox" rel="group" href="images/gallery/fgall01.jpg" style="background: url(images/gallery/fgall01.jpg);"><img src="images/gallery-hover.png" alt=""></a>
+<a class="fancybox" rel="group" href="images/gallery/fgall02.jpg" style="background: url(images/gallery/fgall02.jpg);"><img src="images/gallery-hover.png" alt=""></a>
+<a class="fancybox" rel="group" href="images/gallery/fgall03.jpg" style="background: url(images/gallery/fgall03.jpg);"><img src="images/gallery-hover.png" alt=""></a>
+<a class="fancybox last" rel="group" href="images/gallery/fgall04.jpg" style="background: url(images/gallery/fgall04.jpg);"><img src="images/gallery-hover.png" alt=""></a>
+
 <a class="fancybox" rel="group" href="images/gallery/gall01.jpg" style="background: url(images/gallery/gall01.jpg);"><img src="images/gallery-hover.png" alt=""></a>
 <a class="fancybox" rel="group" href="images/gallery/gall02.jpg" style="background: url(images/gallery/gall02.jpg);"><img src="images/gallery-hover.png" alt=""></a>
 <a class="fancybox" rel="group" href="images/gallery/gall03.jpg" style="background: url(images/gallery/gall03.jpg);"><img src="images/gallery-hover.png" alt=""></a>
